a20a7d194d39dd79dc6c25c36c789f68939e0620
Arafatmollik1 <arafathossain923@gmail.com>
2022-05-27 02:18:23 +0300

    bla bla bla

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  base_url_api: "https://api.all.bdservers.site/"
 };
 
 /*
